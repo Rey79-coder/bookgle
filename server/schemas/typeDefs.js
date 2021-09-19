88d253116fb08f95242944b1dfbbb1c69b51a485
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-09-19 12:38:48 -0500

    typeDefs/resolvers/done

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,52 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+type Book {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+    
+  }
+
   type Query {
-    helloWorld: String
+      me: User
+      users: [User]
+      user(username: String!): User
+      userById(_id: ID!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SavedBookInput): User
+    removeBook(bookId: String!): User
+}
+
+input BookInput {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+}
+
+  type Auth {
+      token: ID!
+      user: User
   }
 `;
 
