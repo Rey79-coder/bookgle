51c55470a315fe3e7d782def840c6e16b1e02ec9
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-09-19 14:10:36 -0500

    added-mutations

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
+  
   const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
